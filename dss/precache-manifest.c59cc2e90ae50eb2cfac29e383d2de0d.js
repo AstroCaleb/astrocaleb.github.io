cafba7a4635d2137d00b120cdfb346f41f319b19
Caleb Dudley <caleb.dudley@gmail.com>
2021-10-17 11:16:14 -0400

    Pushing working project

---
+++ dss/precache-manifest.c59cc2e90ae50eb2cfac29e383d2de0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c20ffea7e6c1152fe918376fb17dd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bd687cd3dd173878a16",
+    "url": "/static/css/2.e106c500.chunk.css"
+  },
+  {
+    "revision": "89a6c58924661b29851f",
+    "url": "/static/css/main.5044eca4.chunk.css"
+  },
+  {
+    "revision": "6bd687cd3dd173878a16",
+    "url": "/static/js/2.eeae0118.chunk.js"
+  },
+  {
+    "revision": "89a6c58924661b29851f",
+    "url": "/static/js/main.7efc539d.chunk.js"
+  },
+  {
+    "revision": "71d215fcad666a341cc2",
+    "url": "/static/js/runtime~main.926864c1.js"
+  }
+]);
